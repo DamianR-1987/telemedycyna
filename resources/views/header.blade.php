6772dcf10b3c04f1a44759547f75624b410557af
Damian R <d.rosinski@tryfon.eu>
2022-03-11 14:49:55 +0100

    Header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,221 @@
+<header>
+<nav class="navbar navbar-expand-xl navbar-light fixed-top bg-white p-4">
+    <div class="container-fluid">
+      <a class="navbar-brand p-0 me-2" href="/" aria-label="Bootstrap">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="d-block my-1" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg>
+      </a>
+      <div class="d-xl-none d-flex">
+        <a class="text-decoration-none nav-link d-xl-none bg-danger fw-bold text-white rounded-pill py-2 px-4" href="#">
+            {{__('ZAMÓW')}}
+        </a>
+        <a class="text-decoration-none nav-link d-xl-none text-primary bg-light rounded-circle mx-2" href="tel:42 000 00 00">
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                </svg>
+            </span>
+        </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark fw-bold mx-1" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Badania i pakiety
+                </a>
+                <ul class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
+                    <li class="pt-3 pb-0 dropdown-item text-break">
+                        <a class="text-decoration-none border-bottom pb-3 d-flex align-items-center row g-0" href="#">
+                            <div class="col-6 flex-shrink-0">
+                                <img class="img-fluid rounded float-start" src="https://www.aptekaolmed.pl/blog_zdjecia/miesnie-kegla-cwiczenia-czym-sa-miesnie-dna-miednicy.jpeg?ver=1646898456" alt="">
+                            </div>
+                            <div class="position-relative col-6 d-flex flex-column flex-grow-1 p-3">
+                                <span class="text-dark fw-bold">Katalog badań
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="position-absolute top-50 end-0 bi bi-chevron-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                </span>
+                                <span class="text-muted">Sprawdź listę badań jakie oferujemy</span>
+                            </div>
+
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Badania domowe</span>
+                            <span class="text-muted">Specjalista wykona badanie na Twojej sofie</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Badania w punkcie</span>
+                            <span class="text-muted">Wygodnie zamów badania do punktu pobrań</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Badania dla dzieci - Poradnik Rodzica</span>
+                            <span class="text-muted">Sprawdź listę badań jakie oferujemy</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Testy wysyłkowe</span>
+                            <span class="text-muted">Kurier dostarczy test, reszta na Twoich warunkach</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark fw-bold mx-1" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dla firm
+                </a>
+                <ul class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
+                    <li class="pt-3 pb-0 dropdown-item text-break">
+                        <a class="text-decoration-none border-bottom pb-3 d-flex align-items-center row g-0" href="#">
+                            <div class="col-6 flex-shrink-0">
+                                <img class="img-fluid rounded float-start" src="https://www.aptekaolmed.pl/blog_zdjecia/miesnie-kegla-cwiczenia-czym-sa-miesnie-dna-miednicy.jpeg?ver=1646898456" alt="">
+                            </div>
+                            <div class="position-relative col-6 d-flex flex-column flex-grow-1 p-3">
+                                <span class="text-dark fw-bold">Testy COVID-19
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="position-absolute top-50 end-0 bi bi-chevron-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                </span>
+                                <span class="text-muted">Zadbaj o bezpieczeństwo swoich pracowników</span>
+                            </div>
+
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Akcje zdrowia dla firm</span>
+                            <span class="text-muted">Zadbaj kompleksowo o zdrowie i wellbeing pracowników</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Kalendarz Akcji Zdrowia</span>
+                            <span class="text-muted">Zaplanuj rok pełen ciekawych inicjatyw prozdrowotnych</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Pakiety badań dla firm</span>
+                            <span class="text-muted">Sprawdź pakiety: Stres, Zmęczenie, Odporność i inne</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Zapisz się na Welletter</span>
+                            <span class="text-muted">Zastrzyk inspiracji i praktycznych porad z obszaru HR, EB i wellbeingu prosto na Twoją skrzynkę</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark fw-bold mx-1" href="#">COVID-19</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark fw-bold mx-1" href="#">Konsultacje</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark fw-bold mx-1" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Kontakt
+                </a>
+                <ul class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
+                    <li class="pt-3 pb-0 dropdown-item text-break">
+                        <a class="text-decoration-none border-bottom pb-3 d-flex align-items-center row g-0" href="#">
+                            <div class="col-6 flex-shrink-0">
+                                <img class="img-fluid rounded float-start" src="https://www.aptekaolmed.pl/blog_zdjecia/miesnie-kegla-cwiczenia-czym-sa-miesnie-dna-miednicy.jpeg?ver=1646898456" alt="">
+                            </div>
+                            <div class="position-relative col-6 d-flex flex-column flex-grow-1 p-3">
+                                <span class="text-dark fw-bold">Kontakt
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="position-absolute top-50 end-0 bi bi-chevron-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                </span>
+                                <span class="text-muted">Masz jakieś pytania? Skontaktuj się z nami</span>
+                            </div>
+
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">O nas</span>
+                            <span class="text-muted">Nasza misja i wizja</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Pomoc</span>
+                            <span class="text-muted"></span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Rekrutacja</span>
+                            <span class="text-muted">Szukamy najlepszych specjalistów</span>
+                        </a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="d-flex flex-column text-decoration-none" href="#">
+                            <span class="text-dark fw-bold">Partnerzy</span>
+                            <span class="text-muted"></span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item d-xl-none">
+                <div class="d-flex flex-column bg-light rounded-3 p-1">
+                    <a class="text-decoration-none nav-link text-dark fw-bold" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                        </svg>
+                        Koszyk
+                    </a>
+                    <a class="text-decoration-none nav-link text-dark fw-bold" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                        </svg>
+                        Moje konto
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item phone-item d-none d-xl-block">
+                <a class="text-decoration-none nav-link text-primary fw-bold mx-3 bg-light rounded-pill" href="tel:42 000 00 00">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                    42 000 00 00
+                    </span>
+                </a>
+            </li>
+            <li class="nav-tem shopping-bag-item d-none d-xl-block">
+                <a class="text-decoration-none nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                    </svg>
+                </a>
+            </li>
+            <li class="nav-tem loggin-item ms-4 me-3 d-none d-xl-block">
+                <a class="text-decoration-none nav-link text-primary bg-light rounded-circle py-2 px-3" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
+                </a>
+            </li>
+            <li class="nav-tem order-item ms-3 d-none d-xl-block">
+                <a class="text-decoration-none nav-link bg-danger fw-bold text-white rounded-pill py-2 px-4" href="#">
+                    {{__('ZAMÓW')}}
+                </a>
+            </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Content</h1>
+@endsection
+
+
